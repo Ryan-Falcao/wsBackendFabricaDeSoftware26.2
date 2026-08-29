bf5628313f874949ec1eb0afc386d9dc79514600
Ryan Falcao <ryanmarques2007f@gmail.com>
2026-08-29 09:37:18 -0300

    feat: criando a primeira view no app

--- projetoApiRest/appApiRest/views.py
+++ projetoApiRest/appApiRest/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from requests import Response
 
-# Create your views here.
+class  ApiStatusView(APIView):
+    def get(self, request):
+        return Response({
+            "status": "online",
+            "message": "API esta rodando normalmente"
+        })
