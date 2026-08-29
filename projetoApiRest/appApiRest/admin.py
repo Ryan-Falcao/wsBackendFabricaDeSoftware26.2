7624c8005a5e6443a0a2ba09744cf8f00b5c6e47
Ryan Falcao <ryanmarques2007f@gmail.com>
2026-08-29 10:30:43 -0300

    feat: adiciona funcao de registro para o admin

--- projetoApiRest/appApiRest/admin.py
+++ projetoApiRest/appApiRest/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Usuario, Jogo, Biblioteca
 
-# Register your models here.
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'email')
+
+@admin.register(Jogo)
+class JogoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'id_externo', 'nome')
+
+@admin.register(Biblioteca)
+class BibliotecaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'usuario', 'jogo')
+ 
