7119b8e802bdd317740f454acff72669cc234b58
Ryan Falcao <ryanmarques2007f@gmail.com>
2026-08-29 09:53:05 -0300

    feat: adiciona modelo de biblioteca

--- projetoApiRest/appApiRest/models.py
+++ projetoApiRest/appApiRest/models.py
@@ -7,3 +7,9 @@ class Usuario(models.Model):
 
     def __str__(self):
         return self.name
+
+class Biblioteca(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='bibliotecas')
+
+    def __str__(self):
+        return f'Biblioteca de {self.usuario.name}'
