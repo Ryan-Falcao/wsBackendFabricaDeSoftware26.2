5d6e405b6adf6493661ef69a2009e79f1217c122
Ryan Falcao <ryanmarques2007f@gmail.com>
2026-08-29 09:42:08 -0300

    fix: corrigindo import errado na view

--- projetoApiRest/appApiRest/views.py
+++ projetoApiRest/appApiRest/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from requests import Response
+from rest_framework.response import Response
 
 class  ApiStatusView(APIView):
     def get(self, request):
