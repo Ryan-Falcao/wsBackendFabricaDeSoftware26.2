eef1447b26ca63d243bd76426e61a2bbbafdb5e5
Ryan Falcao <ryanmarques2007f@gmail.com>
2026-08-29 10:12:51 -0300

    feat: adiciona modelo de jogo

--- projetoApiRest/appApiRest/models.py
+++ projetoApiRest/appApiRest/models.py
@@ -1,15 +1,23 @@
 from django.db import models
 
 class Usuario(models.Model):
-    name = models.CharField(max_length=100)
+    nome = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     senha = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.name
+        return self.nome
 
 class Biblioteca(models.Model):
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='bibliotecas')
+    jogo = models.ForeignKey('Jogo', on_delete=models.CASCADE, related_name='usuarios')
 
     def __str__(self):
-        return f'Biblioteca de {self.usuario.name}'
+         return f"{self.usuario.nome} - {self.jogo.nome}"
+
+class Jogo(models.Model):
+    id_externo = models.IntegerField(unique=True)
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
